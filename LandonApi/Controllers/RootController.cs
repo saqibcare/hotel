713e96d74cc5977db59b7e0329c738ae044e7f59
Saqib Shehzad <saqibcare@gmail.com>
2019-01-04 22:08:54 +0100

    Add project files.

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;  //use this import for the ControllerBase class by which Rootcontroller is inherited
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]  //can be access by simple /
+    [ApiController] //this will tell the Asp.netcore that i am building the controller meant for API
+    public class RootController : ControllerBase
+    {
+        [HttpGet (Name = nameof(GetRoot))] //this will be called by get method
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomController.GetRooms), null)
+                }
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ LandonApi/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSwag.AspNetCore;
+
+namespace LandonApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerDocument();
+
+            // Adding this for the routing
+            services.AddRouting(options => options.LowercaseUrls = true);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+
+                // Add OpenAPI/Swagger middlewares
+                app.UseSwagger(); // Serves the registered OpenAPI/Swagger documents by default on `/swagger/{documentName}/swagger.json`
+                app.UseSwaggerUi3(); // Serves the Swagger UI 3 web ui to view the OpenAPI/Swagger documents by default on `/swagger`
+
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
